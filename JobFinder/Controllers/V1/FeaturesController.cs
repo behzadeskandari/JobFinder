f31ddae47230943a7ebf4fd5e3e1b5a216563b6e
behzad.b.i.g <behzad.b.i.g@gmail.com>
2026-05-14 12:51:56 +0330

    Feature controller

---
+++ JobFinder/Controllers/V1/FeaturesController.cs
@@ -0,0 +1,25 @@
+﻿using JobFinder.Application.Feature.Pricing.Queries.GetFeature;
+using JobFinder.Contracts.Dtos.Feature;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobFinder.Controllers.V1
+{
+    public class FeaturesController : ApiController
+    {
+        [HttpGet("GetFeatures")]
+        public async Task<ActionResult<IEnumerable<FeatureDto>>> GetFeatures()
+        {
+            var result = await Mediator.Send(new GetFeaturesQuery());
+            if (result == null)
+            {
+                return Problem(statusCode: StatusCodes.Status404NotFound, detail: "NotFound");
+            }
+            return Ok(result);
+        }
+
+
+
+    }
+}
